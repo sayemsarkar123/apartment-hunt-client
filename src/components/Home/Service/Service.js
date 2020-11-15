f95c82588cfcaf43d83ecb766a22238a8ea9bb0a
sayemsarkar123 <sayemsarkar123@gmail.com>
2020-11-15 20:59:02 +0600

    added h3 tag

---
+++ src/components/Home/Service/Service.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Service = ({ service: { title, description, img } }) => {
+  const textStyle = { color: '#275a53' };
+  return (
+    <div className="col-md-4">
+      <div className="text-center">
+        <img style={{maxWidth: '70px'}} src={img} alt="" />
+        <h5 style={textStyle} className="font-weight-bold my-3">{title }</h5>
+        <p style={textStyle}>{description}</p>
+        <h1>Hello Word!</h1>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, temporibus?</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
