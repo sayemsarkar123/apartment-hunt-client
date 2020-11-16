4ddc88fee04d27f4559026ae180b1e0525327a13
munnaSorder <developermunnaa@gmail.com>
2020-11-16 12:30:48 +0600

    status dropdown done

--- src/components/Dashboard/BookingList/BookingList.js
+++ src/components/Dashboard/BookingList/BookingList.js
@@ -10,6 +10,25 @@ const BookingList = () => {
         setData(data)
       })
   },[])
+      const handleStatus = (e, id) => {
+      const newStatus = e.target.value
+      const sendData = { id, newStatus }
+      console.log(sendData);
+
+      fetch(`http://localhost:4000/updateStatus`, {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(sendData)
+      })
+          .then(res => res.json())
+          .then(result => {
+              if (result) {
+                  window.alert("Status update")
+              } else {
+                  window.alert("Error update")
+              }
+          })
+  }
   return (
         <div >
             <div className="py-2">
@@ -28,17 +47,41 @@ const BookingList = () => {
                     </thead>
                     <tbody style={{ height: "610px", overflow: 'auto'}}>
                       {
-                        data.map((bookingInfo, index) =>
+                        data.map((bookingInfo) =>
                               <tr style={{fontWeight:'500'}}>
                               <td>{bookingInfo.name}</td>
                               <td>{bookingInfo.email}</td>
                               <td>{bookingInfo.phone}</td>
                               <td style={{ maxWidth: '150px' }}>{bookingInfo.message}</td>
                               <td>
-                                <select className="custom-select mr-5">
-                                  <option value="Pending" defaultValue style={{ color: '#FF4545' }}>{bookingInfo.status}</option>
-                                  <option value="Ongoing">Ongoing</option>
-                                  <option value="Done">Done</option>
+                                <select onChange={(e) => handleStatus(e, bookingInfo._id)} className="custom-select mr-5">
+                                  {
+                                    bookingInfo.status === 'Pending'
+                                    &&
+                                    <>
+                                        <option value="Pending" defaultValue>{bookingInfo.status}</option>
+                                        <option value="Ongoing">Ongoing</option>
+                                        <option value="Done">Done</option>
+                                    </>                                    
+                                  }
+                                  {
+                                    bookingInfo.status === 'Ongoing'
+                                    &&
+                                    <>
+                                      <option value="Ongoing" defaultValue>{bookingInfo.status}</option>
+                                      <option value="Done">Ongoing</option>
+                                      <option value="Pending">Done</option>
+                                    </>
+                                  }
+                                  {
+                                    bookingInfo.status === 'Done'
+                                    &&
+                                    <>
+                                      <option value="Done" defaultValue>{bookingInfo.status}</option>
+                                      <option value="Ongoing">Ongoing</option>
+                                      <option value="Pending">Done</option>
+                                    </>
+                                  }
                                 </select>
                               </td>
                           </tr>
