4c222bbe919d0b0763b2b549bfb1bc4e86d737fe
munnaSorder <developermunnaa@gmail.com>
2020-11-16 14:52:13 +0600

    my rent component add

--- src/components/Dashboard/BookingList/BookingList.js
+++ src/components/Dashboard/BookingList/BookingList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './bookingList.css';
 
 const BookingList = () => {
   const [data, setData] = useState([])
--- src/components/Dashboard/MyRent/MyRent.js
+++ src/components/Dashboard/MyRent/MyRent.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './myRent.css';
 
 const MyRent = () => {
   return (
-    <div>
-      <h1>working...</h1>
-    </div>
+    <div >
+            <div className="py-2">
+               <h4>Add Rent House</h4>
+            </div>
+            <div className="d-flex flex-wrap p-5" style={{ height: '460px', overflow: 'auto', backgroundColor: 'rgb(242 247 255)' }}>
+                <table className="table p-3 bg-white table-hover text-center" >
+                    <thead style={{fontWeight:'500'}}>
+                      <tr style={{fontWeight:'500', backgroundColor:'rgb(214 214 214)'}}>
+                        <th scope="col">House Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody style={{ height: "610px", overflow: 'auto'}}>
+                        <tr style={{fontWeight:'500'}}>
+                          <td>Luxury Vila</td>
+                          <td>$1200</td>
+                          <td className="details-btn btn">View Details</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
   );
 };
 
