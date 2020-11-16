8855000692db8a09842fd0d990b16da50dfe935f
munnaSorder <developermunnaa@gmail.com>
2020-11-16 16:00:44 +0600

    not found & some page add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { createContext, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard/Dashboard';
 import Home from './components/Home/Home/Home';
+import About from './components/Home/Pages/About/About';
+import Concrens from './components/Home/Pages/Concrens/Concrens';
+import Contact from './components/Home/Pages/Contact/Contact';
+import Event from './components/Home/Pages/Event/Event';
+import NotFound from './components/Home/Pages/NotFound/NotFound';
 import Login from './components/Login/Login/Login';
 
 export const LoginContext = createContext();
@@ -12,17 +17,29 @@ const App = () => {
     <LoginContext.Provider value={[user, setUser]}>
       <Router>
         <Switch>
-          <Route path="/login">
-            <Login></Login>
+          <Route exact path="/login">
+            <Login />
           </Route>
           <Route exact path="/">
-            <Home></Home>
+            <Home />
           </Route>
-          <Route path="/dashboard">
-              <Dashboard></Dashboard>
+          <Route exact path="/dashboard">
+              <Dashboard />
+          </Route>
+          <Route exact path="/about">
+              <About />
+          </Route>
+          <Route exact path="/concerns">
+              <Concrens />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/event">
+            <Event />
           </Route>
           <Route path="*">
-
+            <NotFound />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Home/Pages/Concrens/Concrens.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Header/Header';
+
+const Concrens = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>Working....</h1>
+        </div>
+    );
+};
+
+export default Concrens;
