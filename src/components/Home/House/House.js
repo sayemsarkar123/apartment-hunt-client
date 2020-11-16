6946ff893a5c2c4efd345de3c469ea5a2a80ba52
sayemsarkar123 <sayemsarkar123@gmail.com>
2020-11-16 23:32:42 +0600

    Remove Static Data from MyRent Component

--- src/components/Home/House/House.js
+++ src/components/Home/House/House.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-const House = ({ house: { _id, name, price, img } }) => {
+const House = ({ house: { _id, title, price, img, bedroom, bathroom, location } }) => {
+  console.log(img.contentType);
   const history = useHistory();
   const textStyle = {color: '#275a53'}
   return (
     <div className="col-md-4 mt-4">
       <div className="bg-white">
       <div>
-        <img className="img-fluid" src={img} alt={name}/>
+          <img className="img-fluid" src={img.size ? `data:${img.contentType};base64,${img.img}` : img} alt={title}/>
       </div>
       <div className="p-3">
-        <h5 style={textStyle} className="font-weight-bold">{name}</h5>
+        <h5 style={textStyle} className="font-weight-bold">{title}</h5>
         <div className="d-flex align-items-center mt-3">
           <img className="mr-2" style={{maxWidth: '15px'}} src="https://i.ibb.co/8dNZ5M7/map-marker-alt-solid-1.png" alt="" />
-          <p className="m-0 text-secondary">Nasirabad H/S, Chattogram</p>
+            <p className="m-0 text-secondary">{location ? location : 'Nasirabad H/S, Chattogram'}</p>
         </div>
         <div className="d-flex align-items-center mt-2">
           <img className="mr-2" style={{ maxWidth: '15px' }} src="https://i.ibb.co/wpT7GMh/bed-1.png" alt="" />
-          <h6 className="text-secondary m-0">3 Bedrooms</h6>
+            <h6 className="text-secondary m-0">{bedroom ? bedroom : 3} Bedrooms</h6>
           <img className="ml-auto mr-2" style={{ maxWidth: '15px' }} src="https://i.ibb.co/f1XBsjP/bath-1.png" alt="" />
-          <h6 className="text-secondary m-0">2 Bathroom</h6>
+            <h6 className="text-secondary m-0">{bathroom ? bathroom : 2} Bathroom</h6>
         </div>
         <div className="d-flex align-items-center mt-4">
           <h3 style={textStyle} className="m-0 font-weight-bold">{`$${price}`}</h3>
